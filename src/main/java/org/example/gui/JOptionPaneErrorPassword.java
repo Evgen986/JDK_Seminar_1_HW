60a1504ea69518384b39b686009c39b7a0fe40ab
Evgenyi_Malyutin <evgen986@mail.ru>
2023-11-05 17:39:25 +0300

    delete class

--- src/main/java/org/example/gui/JOptionPaneErrorPassword.java
+++
@@ -1,9 +0,0 @@
-package org.example.gui;
-
-import javax.swing.*;
-
-public class JOptionPaneErrorPassword extends JFrame {
-    public JOptionPaneErrorPassword(){
-        setDefaultCloseOperation(HIDE_ON_CLOSE);
-    }
-}
